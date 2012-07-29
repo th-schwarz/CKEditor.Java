ea214959cb3d949f2e6cdf00c98aed5440d0626c
Thilo Schwarz <thilo.schwarz@googlemail.com>
2012-07-29 20:34:16 +0200

    simplified the license terms

--- src/test/java/de/thischwa/ckeditor/util/XHtmlTagToolTest.java
+++ src/test/java/de/thischwa/ckeditor/util/XHtmlTagToolTest.java
@@ -1,23 +1,16 @@
 /*
  * ckeditor.java - Java backend of CKEditor (http://ckeditor.com). 
  * It provides a simple object for creating an editor instance.
+ *
  * Copyright (C) Thilo Schwarz
- * 
- * == BEGIN LICENSE ==
- * 
  * Licensed under the terms of any of the following licenses at your
  * choice:
- * 
+ *
  *  - GNU General Public License Version 2 or later (the "GPL")
  *    http://www.gnu.org/licenses/gpl.html
- * 
- *  - GNU Lesser General Public License Version 2.1 or later (the "LGPL")
- *    http://www.gnu.org/licenses/lgpl.html
- * 
- *  - Mozilla Public License Version 1.1 or later (the "MPL")
- *    http://www.mozilla.org/MPL/MPL-1.1.html
- * 
- * == END LICENSE ==
+ *
+ * - MIT License
+ *   http://en.wikipedia.org/wiki/MIT_License
  */
 package de.thischwa.ckeditor.util;
 
